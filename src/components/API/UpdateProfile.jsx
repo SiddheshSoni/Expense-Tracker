bc992d7b1935166cdbe970a5ba4ebdcca0234f7f
SiddheshSoni <work.siddheshsoni@gmail.com>
2025-11-10 16:15:30 +0530

    auto populate Update Profile for easy edit

--- src/components/API/UpdateProfile.jsx
+++ src/components/API/UpdateProfile.jsx
@@ -5,7 +5,7 @@ const API_key ="AIzaSyC_T56zHD0P7_bI1MwqhUjBNCpO46JlbC8";
 
 const UpdateProfile = async (updates) =>{
     const token = localStorage.getItem('token');
-
+    
     try{    
         const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:update?key=${API_key}`, {
             method: 'POST',
@@ -20,7 +20,7 @@ const UpdateProfile = async (updates) =>{
             }
         });
         const data = await res.json();
-
+        
         if(res.ok){
             console.log(data.idToken);
             return { ok: true, data: data };
@@ -33,7 +33,40 @@ const UpdateProfile = async (updates) =>{
         }
     }catch(err){
         console.log(err);
+        return { ok: false, error: err.message || "A network error occurred." };
     }
 };
 
-export default UpdateProfile;
+const FetchProfile = async () =>{
+    const token = localStorage.getItem('token');
+    
+    try{
+        const res = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${API_key}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                idToken: token,
+                returnSecureToken:true,
+            }),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+        const data = await res.json();
+
+        if(res.ok){
+            return { ok: true, data: data };
+        } else {
+            let errorMessage = "Request Failed";
+            if(data && data.error && data.error.message){
+                errorMessage = data.error.message;
+            }
+            return { ok: false, error: errorMessage };
+        }
+    }catch(err){
+        console.log(err);
+        return { ok: false, error: err.message || "A network error occurred." };
+    }
+    
+}
+
+export { UpdateProfile , FetchProfile};
--- src/components/Pages/ProfilePage.jsx
+++ src/components/Pages/ProfilePage.jsx
@@ -1,12 +1,14 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Alert, Button, Col, Form, FormControl, FormGroup, FormLabel, Row } from 'react-bootstrap'
 import "./ProfilePage.css";
-import UpdateProfile from '../API/UpdateProfile';
+import {UpdateProfile, FetchProfile } from '../API/UpdateProfile';
 
 const ProfilePage = () => {
     const nameRef = useRef();
     const pfpRef = useRef();
     const [error, setError] = useState(null);
+    const [userData, setUserData] = useState(null);
+
     const submitHandler = async (e) => {
         e.preventDefault();
 
@@ -22,8 +24,20 @@ const ProfilePage = () => {
         else{
             console.log(`Profile has succesfully Updated!" }`)
         }
+    };
 
+    const loadData = async ()=>{
+        const result = await FetchProfile();
+        if(!result.ok){
+            setError(result.error);
+        }else{
+            setUserData(result.data.users[0]);
+        }
     }
+    useEffect(()=>{
+        loadData();
+    },[]);
+
   return (
     <>
         <div className='d-flex justify-content-center align-content-center mt-5'> 
@@ -31,11 +45,11 @@ const ProfilePage = () => {
                 <Row className='mb-3'>   
                     <FormGroup>
                         <FormLabel><img src='github-mark.png' /> Full Name:</FormLabel>                       
-                        <FormControl type='text' ref={nameRef}/>
+                        <FormControl type='text' defaultValue={userData? userData.displayName : ''} ref={nameRef}/>
                     </FormGroup>                    
                     <FormGroup>
                         <FormLabel><img src='globe-network.png' /> Profile Photo URL:</FormLabel>
-                        <FormControl type='URL' ref={pfpRef}/>
+                        <FormControl type='URL' defaultValue={ userData? userData.photoUrl : ''} ref={pfpRef}/>
                     </FormGroup>
                 </Row>
                 <Row>
